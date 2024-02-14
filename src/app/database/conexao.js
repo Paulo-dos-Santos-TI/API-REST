93bd99b10ca1e66ae1e19c40f8becad89ae7ad9c
Paulo dos Santos <paulodossantosti@gmail.com>
2024-02-14 11:32:24 -0300

    Refatorar Repository, JSDoc e ajustes finais

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -10,4 +10,22 @@ const conexao = mysql.createConnection({
 
 conexao.connect()
 
+/**
+ * Escura um código sql com ou sem valores
+ * @param {string} sql instrução sql a ser escutada
+ * @param {string=id | [selecao, id]} valores a serem passados para o sql
+ * @param {string} mensagenReject mensagem a ser exibida
+ * @returns objeto da Promisse
+ */
+export const consulta = (sql, valores='', mensagenReject) => {
+  return new Promise((resolve, reject) => {
+      conexao.query(sql, valores, (erro, resultado) => {
+        if(erro) return reject(mensagenReject)
+        // fazer o parse dos resultados
+        const row = JSON.parse(JSON.stringify(resultado))
+        return resolve(row)
+      })
+    })
+}
+
 export default conexao
